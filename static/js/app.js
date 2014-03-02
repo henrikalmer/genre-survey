cf1d4ad7671a358bd63020c78706ef88f091819d
Henrik Almér <henrik.almer@gmail.com>
2014-03-03 00:07:44 +0100

    Consistently use 4 spaces for indentation

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,20 @@
 var playerApp = angular.module('playerApp', [
-	'ngRoute',
-	'playerControllers'
+    'ngRoute',
+    'playerControllers'
 ]);
 
 playerApp.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-			when('/', {
-				templateUrl: 'static/partials/intro.html',
-				controller: 'IntroCtrl'
-			}).
-			when('/play/', {
-				templateUrl: 'static/partials/play.html',
-				controller: 'PlayerCtrl'
-			}).
-			otherwise({
-				redirectTo: '/'
-			});
-	}]);
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'static/partials/intro.html',
+                controller: 'IntroCtrl'
+            }).
+            when('/play/', {
+                templateUrl: 'static/partials/play.html',
+                controller: 'PlayerCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,18 +1,18 @@
 var playerControllers = angular.module('playerControllers', []);
 
 playerControllers.controller('IntroCtrl', function ($scope) {
-	$scope.heading = 'Välkommen!';
-	$scope.intro = 'Lorem ipsum yada yada';
-	$scope.btn_text = 'Start';
+    $scope.heading = 'Välkommen!';
+    $scope.intro = 'Lorem ipsum yada yada';
+    $scope.btn_text = 'Start';
 });
 
 playerControllers.controller('PlayerCtrl', function ($scope) {
-	$scope.heading = 'Lyssna!';
-	$scope.genres = [
-		{'name': 'Pop', 'key': 'pop', 'value': 0},
-		{'name': 'Rock', 'key': 'rock', 'value': 0},
-		{'name': 'Reggae', 'key': 'reggae', 'value': 0},
-		{'name': 'Jazz', 'key': 'jazz', 'value': 0},
-		{'name': 'Klassiskt', 'key': 'classical', 'value': 0},
-	];
+    $scope.heading = 'Lyssna!';
+    $scope.genres = [
+        {'name': 'Pop', 'key': 'pop', 'value': 0},
+        {'name': 'Rock', 'key': 'rock', 'value': 0},
+        {'name': 'Reggae', 'key': 'reggae', 'value': 0},
+        {'name': 'Jazz', 'key': 'jazz', 'value': 0},
+        {'name': 'Klassiskt', 'key': 'classical', 'value': 0},
+    ];
 });
