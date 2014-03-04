b5767d2edf029ea56cb683bc8c72043238e374ff
Henrik Almér <henrik.almer@gmail.com>
2014-03-04 18:33:39 +0100

    Add creation timestamp to model

--- survey/models.py
+++ survey/models.py
@@ -1,8 +1,10 @@
+import datetime
 from survey import db
 
 class Response(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     json = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def __init__(self, json_string):
         self.json = json_string
